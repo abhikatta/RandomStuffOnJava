33998d86edb7ba6990912434b0eb664a0d3258b0
AbhinayKatta <76813100+AbhinayKatta@users.noreply.github.com>
2021-05-29 20:16:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+// // CASTING:
+// public class Main{
+//     public static void main(String[] args) {
+//         double x = 23.12;
+//         int y = (int)x + 12;
+//             //      V
+//             //   This is called as casting.
+//             // We basically cast a data type to another data type 
+//             // Here the data type double is "casted" into a int type        
+// System.out.println(y);
+//     }
+// }
+// //CASTING USING WRAPPER CLASS:
+// public class Main{
+//     public static void main(String[] args) {
+//         String x = "23";    
+//         // here a string is converted into a numeric value using the 
+//         // function dataType.parseDataType(name);
+//         // This is used when input is given as a string and we need to onvert it to a value
+//         int y = Integer.parseInt(x)+ 2;
+//         System.out.println(y);
+//     }
+// }
+//  // MATH FUNCTIONS:
+// public class Main {
+//     public static void main(String[] args) {
+//         float result =(float)Math.ceil(23.23D);
+//         System.out.println(result); 
+//         int result1 = (int) Math.ceil(24.4D);
+//         System.out.println(result1);
+//     }
+// }
